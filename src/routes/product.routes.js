a447e48222e32abaaa1061713dda277aa630e32c
RayJustin24 <villanuevarayjustin44@gmail.com>
2024-07-16 17:02:21 +0800

    updated

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -3,37 +3,15 @@ const { createValidator } = require('express-joi-validation');
 const productController = require('../controllers/product.controller');
 const createProductSchema = require('../validation/create.product.validation');
 const updateProductSchema = require('../validation/update.product.validation');
-<<<<<<< HEAD
 const authenticateJWT = require('../middleware/authMiddleware');
-=======
-<<<<<<< HEAD
-const authenticateJWT = require('../middleware/authMiddleware');
-=======
-const authenticate = require('../middlewares/authenticate');
->>>>>>> 3dddfb58f4fb900dc016069a2b2f7c092ab02bc5
->>>>>>> 82f9835e080cb809d7948e2c63bbf24afbb5b1a9
 
 const validator = createValidator({});
 const router = express.Router();
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 82f9835e080cb809d7948e2c63bbf24afbb5b1a9
 router.post('/products', authenticateJWT, validator.body(createProductSchema), productController.createProduct);
 router.get('/products', authenticateJWT, productController.getAllProducts);
 router.get('/products/:id', authenticateJWT, productController.getProductById);
 router.put('/products/:id', authenticateJWT, validator.body(updateProductSchema), productController.updateProduct);
 router.delete('/products/:id', authenticateJWT, productController.deleteProduct);
-<<<<<<< HEAD
-=======
-=======
-router.post('/products', authenticate, validator.body(createProductSchema), productController.createProduct);
-router.get('/products', authenticate, productController.getAllProducts);
-router.get('/products/:id', authenticate, productController.getProductById);
-router.put('/products/:id', authenticate, validator.body(updateProductSchema), productController.updateProduct);
-router.delete('/products/:id', authenticate, productController.deleteProduct);
->>>>>>> 3dddfb58f4fb900dc016069a2b2f7c092ab02bc5
->>>>>>> 82f9835e080cb809d7948e2c63bbf24afbb5b1a9
 
 module.exports = router;
