87cb79dd742093c3ec02143b17f4dd2605e0ec20
RayJustin24 <villanuevarayjustin44@gmail.com>
2024-07-16 15:10:14 +0800

    updated

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -3,15 +3,16 @@ const { createValidator } = require('express-joi-validation');
 const productController = require('../controllers/product.controller');
 const createProductSchema = require('../validation/create.product.validation');
 const updateProductSchema = require('../validation/update.product.validation');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 const validator = createValidator({});
 
 const router = express.Router();
 
-router.post('/products', validator.body(createProductSchema), productController.createProduct);
-router.get('/products', productController.getAllProducts);
-router.get('/products/:id', productController.getProductById);
-router.put('/products/:id', validator.body(updateProductSchema), productController.updateProduct);
-router.delete('/products/:id', productController.deleteProduct);
+router.post('/products', authMiddleware, validator.body(createProductSchema), productController.createProduct);
+router.get('/products', authMiddleware, productController.getAllProducts);
+router.get('/products/:id', authMiddleware, productController.getProductById);
+router.put('/products/:id', authMiddleware, validator.body(updateProductSchema), productController.updateProduct);
+router.delete('/products/:id', authMiddleware, productController.deleteProduct);
 
 module.exports = router;
