8a5c95141d7a7a6544217ca4418704c940c08a1c
RayJustin24 <113017749+RayJustin24@users.noreply.github.com>
2024-07-29 14:01:57 +0800

    Delete src directory

--- src/config/db.config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    HOST: process.env.DB_HOST || 'localhost',
-    USER: process.env.DB_USER,
-    PASSWORD: process.env.DB_PASSWORD,
-    DB: process.env.DB_NAME,
-    dialect: "mysql"
-};
--- src/utils/authUtils.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateToken = (user) => {
-    const payload = { id: user.id, username: user.username };
-    return jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: '1h' });
-};
-
-module.exports = { generateToken };
